414925d0246872bd76ca18648ac6b938c51ebd18
Sandy F <sandyforrest001@outlook.com>
2023-12-04 17:17:56 +0100

    04p2: Optimised day 4

--- day04/solution2.py
+++ day04/solution2.py
@@ -1,13 +1,12 @@
+# put the input into inputArray, not strictly necessary but makes life easier
 import os
-import math
 currentPath = os.path.normpath(os.path.realpath(os.path.split(__file__)[0]))
 filename = os.path.join(currentPath, "input.txt")
-
 with open(filename, 'r') as f:
     inputArray = f.readlines()
 
+# parse inputs and convert to a single scratchCard dictionary
 scratchCard = {}
-
 for line in inputArray:
     thingNumber    = int(line.split(':')[0].split(' ')[-1])
     line           = line.split(':')[1].strip('\n')
@@ -15,30 +14,14 @@ for line in inputArray:
     chosenNumbers  = [int(i) for i in line.split('|')[1].split(' ') if i != '']
     scratchCard[thingNumber] = (winningNumbers, chosenNumbers, 1)
 
-# parse inputs to get lists of winning and chosen numbers
+# unpack each item in scratchCard dict, find number of matches
+# then increment the timesToRun of subsequent cards by the timesToRun of the current card
 for index in scratchCard:
     winningNumbers, chosenNumbers, timesToRun = scratchCard[index]
-    print(f"{winningNumbers} | {chosenNumbers}, run {timesToRun} time(s)")
-
-    # find winning numbers
-    cardWins = 0
-    for chosenNumber in chosenNumbers:
-        if chosenNumber in winningNumbers:
-            cardWins += 1
-            print(f"{chosenNumber} yes it's in, score is now {cardWins}")
-        else:
-            print(f"{chosenNumber} ISN'T, score is now {cardWins}")
-    print(cardWins)
-
+    cardWins = sum(int(chosenNumber in winningNumbers) for chosenNumber in chosenNumbers)
     for i in range(1, cardWins + 1):
-        scratchCard[index + i] = (scratchCard[index + i][0], scratchCard[index + i][1], scratchCard[index + i][2] + timesToRun)
+        scratchCard[index + i] =  (scratchCard[index + i][0], scratchCard[index + i][1], scratchCard[index + i][2] + timesToRun)
 
-totalScore = 0
-
-
-for index in scratchCard:
-    _, _, timesToRun = scratchCard[index]
-    totalScore += timesToRun
-    print("Score is ", totalScore)
-
+# add up all timesToRun values of the scratchcards
+totalScore = sum(scratchCard[index][2] for index in scratchCard)
 print(totalScore)
