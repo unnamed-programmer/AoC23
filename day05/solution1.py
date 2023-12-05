570bbf5fc0e360ef77db7e01880e474f6472bbc6
Sandy F <sandyforrest001@outlook.com>
2023-12-05 13:00:56 +0100

    05p1: it works!

--- day05/solution1.py
+++ day05/solution1.py
@@ -1,6 +1,6 @@
 import os
 currentPath = os.path.normpath(os.path.realpath(os.path.split(__file__)[0]))
-filename = os.path.join(currentPath, "testin.txt")
+filename = os.path.join(currentPath, "input.txt")
 
 with open(filename, 'r') as f:
     inputArray = f.readlines()
@@ -32,46 +32,140 @@ seeds = [int(i) for i in
         .split(' ')
         if i]
 
+
+
+# Parse inputs and generate map table
+# change from [destStart, srcStart, range]
+# to [srcStart, srcEnd, offset]
+
 index += 3
 seedsToSoilMap = []
 while inputArray[index] != '\n':
-    seedsToSoilMap.append([int(i) for i in inputArray[index].strip('\n').split(' ') if i])
+    temp = [int(i) for i in inputArray[index].strip('\n').split(' ') if i]
+    seedsToSoilMap.append([temp[1], temp[1] + temp[2], temp[0] - temp[1]])
     index += 1
 
 index += 2
 soilToFertilizerMap = []
 while inputArray[index] != '\n':
-    soilToFertilizerMap.append([int(i) for i in inputArray[index].strip('\n').split(' ') if i])
+    temp = [int(i) for i in inputArray[index].strip('\n').split(' ') if i]
+    soilToFertilizerMap.append([temp[1], temp[1] + temp[2], temp[0] - temp[1]])
     index += 1
 
 index += 2
 fertilizerToWaterMap = []
 while inputArray[index] != '\n':
-    fertilizerToWaterMap.append([int(i) for i in inputArray[index].strip('\n').split(' ') if i])
+    temp = [int(i) for i in inputArray[index].strip('\n').split(' ') if i]
+    fertilizerToWaterMap.append([temp[1], temp[1] + temp[2], temp[0] - temp[1]])
     index += 1
 
 index += 2
 waterToLightMap = []
 while inputArray[index] != '\n':
-    waterToLightMap.append([int(i) for i in inputArray[index].strip('\n').split(' ') if i])
+    temp = [int(i) for i in inputArray[index].strip('\n').split(' ') if i]
+    waterToLightMap.append([temp[1], temp[1] + temp[2], temp[0] - temp[1]])
     index += 1
 
 index += 2
 lightToTemperatureMap = []
 while inputArray[index] != '\n':
-    lightToTemperatureMap.append([int(i) for i in inputArray[index].strip('\n').split(' ') if i])
+    temp = [int(i) for i in inputArray[index].strip('\n').split(' ') if i]
+    lightToTemperatureMap.append([temp[1], temp[1] + temp[2], temp[0] - temp[1]])
     index += 1
 
 index += 2
 temperatureToHumidityMap = []
 while inputArray[index] != '\n':
-    temperatureToHumidityMap.append([int(i) for i in inputArray[index].strip('\n').split(' ') if i])
+    temp = [int(i) for i in inputArray[index].strip('\n').split(' ') if i]
+    temperatureToHumidityMap.append([temp[1], temp[1] + temp[2], temp[0] - temp[1]])
     index += 1
 
 index += 2
 humidityToLocationMap = []
 while inputArray[index] != '\n':
-    humidityToLocationMap.append([int(i) for i in inputArray[index].strip('\n').split(' ') if i])
+    temp = [int(i) for i in inputArray[index].strip('\n').split(' ') if i]
+    humidityToLocationMap.append([temp[1], temp[1] + temp[2], temp[0] - temp[1]])
     index += 1
 
-raise RuntimeError('FUCKET ERROR - Invalid temperature temperature output')
+print("Finished creating map arrays")
+
+
+# Map seeds to final results
+mappedSeeds = [[i] for i in seeds]
+for seed in mappedSeeds:
+    seedIndex = 0
+    for seedMap in seedsToSoilMap:
+        if seed[seedIndex] >= seedMap[0] and seed[seedIndex] <= seedMap[1]:
+            seed.append(seed[seedIndex] + seedMap[2])
+            print(f"{seed[seedIndex]} maps to {seed[seedIndex + 1]} in {seedMap}")
+        else:
+            print(f"{seed[seedIndex]} does NOT map in {seedMap}")
+    if len(seed) == seedIndex + 1:
+        seed.append(seed[seedIndex])
+
+    seedIndex += 1
+    for seedMap in soilToFertilizerMap:
+        if seed[seedIndex] >= seedMap[0] and seed[seedIndex] <= seedMap[1]:
+            seed.append(seed[seedIndex] + seedMap[2])
+            print(f"{seed[seedIndex]} maps to {seed[seedIndex + 1]} in {seedMap}")
+        else:
+            print(f"{seed[seedIndex]} does NOT map in {seedMap}")
+    if len(seed) == seedIndex + 1:
+        seed.append(seed[seedIndex])
+
+    seedIndex += 1
+    for seedMap in fertilizerToWaterMap:
+        if seed[seedIndex] >= seedMap[0] and seed[seedIndex] <= seedMap[1]:
+            seed.append(seed[seedIndex] + seedMap[2])
+            print(f"{seed[seedIndex]} maps to {seed[seedIndex + 1]} in {seedMap}")
+        else:
+            print(f"{seed[seedIndex]} does NOT map in {seedMap}")
+    if len(seed) == seedIndex + 1:
+        seed.append(seed[seedIndex])
+
+    seedIndex += 1
+    for seedMap in waterToLightMap:
+        if seed[seedIndex] >= seedMap[0] and seed[seedIndex] <= seedMap[1]:
+            seed.append(seed[seedIndex] + seedMap[2])
+            print(f"{seed[seedIndex]} maps to {seed[seedIndex + 1]} in {seedMap}")
+        else:
+            print(f"{seed[seedIndex]} does NOT map in {seedMap}")
+    if len(seed) == seedIndex + 1:
+        seed.append(seed[seedIndex])
+
+    seedIndex += 1
+    for seedMap in lightToTemperatureMap:
+        if seed[seedIndex] >= seedMap[0] and seed[seedIndex] <= seedMap[1]:
+            seed.append(seed[seedIndex] + seedMap[2])
+            print(f"{seed[seedIndex]} maps to {seed[seedIndex + 1]} in {seedMap}")
+        else:
+            print(f"{seed[seedIndex]} does NOT map in {seedMap}")
+    if len(seed) == seedIndex + 1:
+        seed.append(seed[seedIndex])
+
+    seedIndex += 1
+    for seedMap in temperatureToHumidityMap:
+        if seed[seedIndex] >= seedMap[0] and seed[seedIndex] <= seedMap[1]:
+            seed.append(seed[seedIndex] + seedMap[2])
+            print(f"{seed[seedIndex]} maps to {seed[seedIndex + 1]} in {seedMap}")
+        else:
+            print(f"{seed[seedIndex]} does NOT map in {seedMap}")
+    if len(seed) == seedIndex + 1:
+        seed.append(seed[seedIndex])
+
+    seedIndex += 1
+    for seedMap in humidityToLocationMap:
+        if seed[seedIndex] >= seedMap[0] and seed[seedIndex] <= seedMap[1]:
+            seed.append(seed[seedIndex] + seedMap[2])
+            print(f"{seed[seedIndex]} maps to {seed[seedIndex + 1]} in {seedMap}")
+        else:
+            print(f"{seed[seedIndex]} does NOT map in {seedMap}")
+    if len(seed) == seedIndex + 1:
+        seed.append(seed[seedIndex])
+
+lowestNumber = 99999999999999999
+for mappedSeed in mappedSeeds:
+    if mappedSeed[-1] < lowestNumber:
+        lowestNumber = mappedSeed[-1]
+
+print(lowestNumber)
