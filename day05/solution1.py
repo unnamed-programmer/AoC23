9fce0ba3a73a7158977aa3816e098b84aadf5737
Sandy F <sandyforrest001@outlook.com>
2023-12-05 12:24:56 +0100

    05p1: it's on the way

--- day05/solution1.py
+++ day05/solution1.py
@@ -1,6 +1,6 @@
 import os
 currentPath = os.path.normpath(os.path.realpath(os.path.split(__file__)[0]))
-filename = os.path.join(currentPath, "input.txt")
+filename = os.path.join(currentPath, "testin.txt")
 
 with open(filename, 'r') as f:
     inputArray = f.readlines()
@@ -22,3 +22,56 @@ with open(filename, 'r') as f:
 # cascade references - hppefully end up with locations for each of the seeds
 
 # find lowest location number corresponding to a seed
+
+# seeds list
+index = 0
+seeds = [int(i) for i in
+        inputArray[index]
+        .split(':')[-1]
+        .strip('\n')
+        .split(' ')
+        if i]
+
+index += 3
+seedsToSoilMap = []
+while inputArray[index] != '\n':
+    seedsToSoilMap.append([int(i) for i in inputArray[index].strip('\n').split(' ') if i])
+    index += 1
+
+index += 2
+soilToFertilizerMap = []
+while inputArray[index] != '\n':
+    soilToFertilizerMap.append([int(i) for i in inputArray[index].strip('\n').split(' ') if i])
+    index += 1
+
+index += 2
+fertilizerToWaterMap = []
+while inputArray[index] != '\n':
+    fertilizerToWaterMap.append([int(i) for i in inputArray[index].strip('\n').split(' ') if i])
+    index += 1
+
+index += 2
+waterToLightMap = []
+while inputArray[index] != '\n':
+    waterToLightMap.append([int(i) for i in inputArray[index].strip('\n').split(' ') if i])
+    index += 1
+
+index += 2
+lightToTemperatureMap = []
+while inputArray[index] != '\n':
+    lightToTemperatureMap.append([int(i) for i in inputArray[index].strip('\n').split(' ') if i])
+    index += 1
+
+index += 2
+temperatureToHumidityMap = []
+while inputArray[index] != '\n':
+    temperatureToHumidityMap.append([int(i) for i in inputArray[index].strip('\n').split(' ') if i])
+    index += 1
+
+index += 2
+humidityToLocationMap = []
+while inputArray[index] != '\n':
+    humidityToLocationMap.append([int(i) for i in inputArray[index].strip('\n').split(' ') if i])
+    index += 1
+
+raise RuntimeError('FUCKET ERROR - Invalid temperature temperature output')
