be2bac60e6d9d3a5fdd7aa0236b8b665ffa2120b
Sandy F <sandyforrest001@outlook.com>
2023-12-07 20:10:26 +0000

    07p1: did it!

--- day07/solution1.py
+++ day07/solution1.py
@@ -1,48 +1,61 @@
+def gradeCard(card: list[int]) -> int:
+    # return (grade, digit0, digit1..4) in int form, g01234
+    mostCommon = max(card, key=card.count)
+    maxFrequency = card.count(mostCommon)
+    cardCopy = [c for c in card if c != mostCommon]
+    result = ''
+    match maxFrequency:
+        case 5:
+            result = '7'
+        case 4:
+            result = '6'
+        case 3:
+            secondMaxFrequency = cardCopy.count( max(cardCopy, key=cardCopy.count) )
+            match secondMaxFrequency:
+                case 2:
+                    result = '5'
+                case _:
+                    result = '4'
+        case 2:
+            secondMaxFrequency = cardCopy.count( max(cardCopy, key=cardCopy.count) )
+            match secondMaxFrequency:
+                case 2:
+                    result = '3'
+                case _:
+                    result = '2'
+        case 1:
+            result = '1'
+        case _:
+            raise RuntimeError("DON'T PANIC")
+
+    returnValue = result
+    for i in card:
+        returnValue += f'0{i}' if i < 10 else f'{i}'
+    return int(returnValue)
+
 import os
 currentPath = os.path.normpath(os.path.realpath(os.path.split(__file__)[0]))
-filename = os.path.join(currentPath, "testin.txt")
+filename = os.path.join(currentPath, "input.txt")
 with open(filename, 'r') as f:
     inputArray = [i.strip() for i in f.readlines()]
 givenHands = [i.split(' ') for i in inputArray]
+organisedHands = []
 
 for hand in givenHands:
     cardToIntDict = {'A': 14, 'K': 13, 'Q': 12, 'J': 11, 'T': 10, '9': 9, '8': 8, '7': 7, '6': 6, '5': 5, '4': 4, '3': 3, '2': 2}
-    card = [cardToIntDict[i] for i in hand[0]]
+    cards = [cardToIntDict[i] for i in hand[0]]
     bid = int(hand[1])
-    print(f'{card}, {bid}')
+    grade = gradeCard(cards)
+    organisedHands.append([cards, bid, grade])
 
+organisedHands.sort(key=lambda x: x[2])
 
+total = 0
 
+for index, hand in enumerate(organisedHands):
+    cards = hand[0]
+    bid = hand[1]
+    grade = hand[2]
+    total += bid * (index + 1)
 
-def gradeCard(card: list[int]) -> list[int]:
-    # return (grade, digit0, digit1..4) in int form, g01234
-    returnValue = []
-    mostCommon = max(card, key=card.count)
-    if card.count(mostCommon) == 5:
-        returnValue.append(7)
-    elif card.count(mostCommon) == 4:
-        returnValue.append(6)
-    elif card.count(mostCommon) == 3:
-        cardCopy = card
-        while mostCommon in cardCopy: cardCopy.remove(mostCommon)
-        secMostCommon = max(cardCopy, key=cardCopy.count)
-        if cardCopy.count(secMostCommon) == 2:
-            returnValue.append(5)
-        else:
-            returnValue.append(4)
-    elif card.count(mostCommon) == 2:
-        cardCopy = card
-        while mostCommon in cardCopy: cardCopy.remove(mostCommon)
-        secMostCommon = max(cardCopy, key=cardCopy.count)
-        if cardCopy.count(secMostCommon) == 2:
-            returnValue.append(3)
-        else:
-            returnValue.append(2)
-    elif card.count(mostCommon) == 1:
-        returnValue.append(1)
-    else:
-        raise RuntimeError("DON'T PANIC")
-
-    returnValue.extend(iter(card))
-    
-    return returnValue
+print(total)
