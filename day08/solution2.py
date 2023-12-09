7cf16c787f50fd4bab2bb0952d1c13a5c10a6bef
Sandy F <sandyforrest001@outlook.com>
2023-12-09 21:46:03 +0000

    08p2 rerun: Redid it and it works now!

--- day08/solution2.py
+++ day08/solution2.py
@@ -1,4 +1,19 @@
 import os
+import math
+from functools import reduce
+
+def getPrimeFactors(n: int) -> list[int]:
+    factors = []
+    i = 2
+    while i ** 2 <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return factors
 
 def getInput():
     currentPath = os.path.normpath(os.path.realpath(os.path.split(__file__)[0]))
@@ -33,10 +48,22 @@ for startItem in answersEndingWithA:
         for direction in directions:
             element = elementMap[answer]
             answer = element[0] if direction == 'L' else element[1]
-            if answer == 'ZZZ': continuing = False
             currentTries += 1
-            # print(answer)
+            if answer[-1] == 'Z': continuing = False; break
             if currentTries >= 100000: raise RuntimeError
     tries.append(currentTries)
 
-print(tries)
+# find LCM: split into prime factors, then multiply the common ones up?
+primeFactors = [getPrimeFactors(number) for number in tries]
+
+tempPF = []
+for primeFactor in primeFactors:
+    if isinstance(primeFactor, int):
+        tempPF.append(primeFactor)
+    else:
+        tempPF.extend(iter(primeFactor))
+primeFactors = tempPF
+
+lowComMul = reduce(lambda x, y: math.lcm(x, y), primeFactors)
+
+print(lowComMul)
