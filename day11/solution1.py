c2baa7858dda93af5341f011d984543894d85bc5
Sandy F <sandyforrest001@outlook.com>
2023-12-11 10:24:43 +0000

    11p1: it works!

--- day11/solution1.py
+++ day11/solution1.py
@@ -6,18 +6,31 @@ def getInput():
         inputArray = f.readlines()
     return inputArray
 
+import itertools
 inputArray = [i.strip() for i in getInput()]
 width = len(inputArray[0])
 height = len(inputArray)
 
 emptyRows = [i for i in range(width)  if all(inputArray[i][j] == '.' for j in range(height))][::-1]
-emptyCols = [i for i in range(height) if all(inputArray[j][i] == '.' for j in range(width))][::-1]
+emptyCols = [i for i in range(height) if all(inputArray[j][i] == '.' for j in range(width) )][::-1]
 
 for col in emptyCols:
     inputArray = [f'{i[:col]}.{i[col:]}' for i in inputArray]
 width = len(inputArray[0])
 for row in emptyRows:
     inputArray.insert(row, '.' * width)
+height = len(inputArray)
 
+galaxies = [(x, y)
+            for y, x in itertools.product(range(height), range(width))
+            if inputArray[y][x] == '#']
+
+galaxyPairs = list(itertools.combinations(galaxies, 2))
+
+distanceSum = 0
+for pair in galaxyPairs:
+    distance = (abs(pair[0][0] - pair[1][0])) + (abs(pair[0][1] - pair[1][1]))
+    distanceSum += distance
 
 print('dingus')
+print(distanceSum)
